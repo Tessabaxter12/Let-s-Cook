2752a4fc5b59081b7d74f2834ed28b433523b354
Tessabaxter12 <tessabaxter12@gmail.com>
2024-01-05 20:40:52 -0800

    api key finally working

--- controllers/recipeControllers.js
+++ controllers/recipeControllers.js
@@ -1,14 +1,32 @@
 //Import Dependencies
 const express = require('express')
 const axios = require('axios')
-const allPlacesUrl = process.env.COUNTRY_API_URL
-const nameSearchBaseUrl = process.env.C_BY_NAME_BASE_URL
-const Recipe = require('../models/recipe')
+const apiUrlFront = process.env.SEARCH_BY_INGREDIENTS_API_URL_BASE
+const apikey = process.env.API_KEY
+const apiUrlRecipe = process.env.RECIPE_API_URL
 
 //Create Router
 const router = express.Router()
 
 //Routes + Controllers
+// GET -> /Search/allRecipes
+router.get('/all', (req, res) => {
+    const { username, loggedIn, userId } = req.session
+    // we have to make our api call
+    axios(`${apiUrlRecipe}?apiKey=${apikey}`)
+        // if we get data, render an index page
+        .then(apiRes => {
+            console.log('this came back from the api: \n', apiRes.data[0])
+            // apiRes.data is an array of counvtry objects
+            //res.send(apiRes.data)
+            res.render('recipes/search', { recipes: apiRes.data, username, userId, loggedIn})
+        })
+        // if something goes wrong, display an error page
+        .catch(err => {
+            console.log('error')
+            res.redirect(`/error?error=${err}`)
+        })
+})
 
 //Export Router
 module.exports = router
--- models/recipe.js
+++ models/recipe.js
@@ -1,5 +1,24 @@
-//Our Schema and dependencies
+//Our Schema and dependencies 
 //const mongoose = require('../utils/connection')
 
 // destructuring the Schema and model from mongoose
 //const { Schema, model } = mongoose
+
+//Schema definition
+//const recipeSchema = new Schema({
+//    id: { type: String, required: true },
+//    name: { type: String, required: true },
+//    owner: {
+//        type: Schema.Types.ObjectId,
+//        ref: 'User',
+//        required: true
+//    }
+//}, {
+//    timestamps: true
+//})
+
+//create user model
+//const Recipe = model('Recipe', recipeSchema)
+
+//export user model
+//module.exports = Recipe
